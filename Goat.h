4b8a2663426d45a5021f39c2e51b0e955ced6ed6
etdil123 <ethandilk@MacBook-Air.attlocal.net>
2024-10-26 20:35:32 -0700

    returned next day - only change is adding overloaded < operator that I forgot to include

--- Goat.h
+++ Goat.h
@@ -28,6 +28,11 @@ public:
     string get_color() const        { return color; }
 
     // write overloaded < operator for the std::list
+    bool operator< (const Goat &other) const {
+        return name < other.name;
+
+    }
+
 };
 
 #endif
